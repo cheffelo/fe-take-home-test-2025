26bbe30871d4da31d0d0c3ac55568cdc46f28ee9
Martin <martin.jakobsen@cheffelo.com>
2025-03-07 14:58:57 +0100

    Avoid optional chaining on Listings.tsx

--- components/Listings.tsx
+++ components/Listings.tsx
@@ -10,12 +10,16 @@ interface ListingsProps extends HTMLAttributes<HTMLUListElement> {}
 export const Listings = (props: ListingsProps) => {
   const { data: listings } = useGetListingsQuery();
 
+  if (!listings) {
+    return null;
+  }
+
   return (
     <ul
       className="grid items-stretch grid-cols-2 gap-4 md:grid-cols-4"
       {...props}
     >
-      {listings?.data.map((listing) => (
+      {listings.data.map((listing) => (
         <ListingCard key={listing.id} listing={listing} />
       ))}
     </ul>
